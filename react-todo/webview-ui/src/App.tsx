38608f6641daa29af5026749cf676c25038e753b
FVPukay <28515879+FVPukay@users.noreply.github.com>
2025-02-20 20:45:48 -0800

    Add final code to complete the extension

--- react-todo/webview-ui/src/App.tsx
+++ react-todo/webview-ui/src/App.tsx
@@ -1,20 +1,28 @@
-import { VSCodeButton } from "@vscode/webview-ui-toolkit/react";
-import { vscode } from "./assets/vscode";
+import { vscode } from "./vscode";
+import { useEffect } from "react";
+import InputBox from "./components/InputBox";
+import TodoItem from "./components/TodoItem";
+import { useTodo } from "./contexts/TodoContext";
 
 function App() {
+  const { todos } = useTodo();
 
-  function handleHowdyClick() {
+  useEffect(() => {
     vscode.postMessage({
       command: "ready",
-      text: "Hey there partner! 🤠",
     });
-  }
+  }, []);
+
   return (
-    <main>
-      <h1>Hello World!</h1>
-      <VSCodeButton onClick={handleHowdyClick}>Howdy!</VSCodeButton>
-    </main>
+    <div className="h-screen  flex flex-col  items-center p-10 gap-5">
+      <InputBox />
+      <ul className="max-h-96 overflow-y-auto py-2 px-2">
+        {todos.map((todoItem) => (
+          <TodoItem key={todoItem.id} {...todoItem} />
+        ))}
+      </ul>
+    </div>
   );
 }
 
-export default App
+export default App;
